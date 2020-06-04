0e8a33763e99b6d15b37874f5cff1ee28afa59db
Andres castillo <cato.castques@gmail.com>
2020-06-04 04:47:39 -0600

    initial setup. Need to setup real flow with gulp etc

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # CATIX MINDFUL JOY WP THEME
 
 Gulp. Custom.
+
+## TO Do
+
+* warning gulp-util@3.0.8: gulp-util is deprecated - replace it, following the guidelines at https://medium.com/gulpjs/gulp-util-ca3b1f9f9ac5
---
+++ gulpfile.js
@@ -0,0 +1,112 @@
+// Gulp.js configuration
+'use strict';
+
+const
+
+// source and build folders
+    dir = {
+        src: './assets/',
+        build: './public/'
+    },
+
+    // Gulp and plugins
+    gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    newer = require('gulp-newer'),
+    imagemin = require('gulp-imagemin'),
+    sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    deporder = require('gulp-deporder'),
+    concat = require('gulp-concat'),
+    stripdebug = require('gulp-strip-debug'),
+    uglify = require('gulp-uglify');
+
+// Browser-sync
+var browsersync = false;
+
+
+// PHP settings
+const php = {
+    src: '**/*.php',
+    build: dir.build
+};
+
+// image settings
+const images = {
+    src: dir.src + 'img/**/*',
+    build: dir.build + 'images/'
+};
+
+// image processing
+gulp.task('images', () => {
+    return gulp.src(images.src)
+        .pipe(newer(images.build))
+        .pipe(imagemin())
+        .pipe(gulp.dest(images.build));
+});
+
+
+// CSS settings
+var css = {
+    src: dir.src + 'scss/style.scss',
+    watch: dir.src + 'scss/**/*',
+    build: dir.build,
+    sassOpts: {
+        outputStyle: 'nested',
+        imagePath: images.build,
+        precision: 3,
+        errLogToConsole: true
+    },
+    processors: [
+        require('postcss-assets')({
+            loadPaths: ['img/'],
+            basePath: dir.build,
+            baseUrl: ''
+        }),
+        require('autoprefixer')({
+            browsers: ['last 2 versions', '> 2%']
+        }),
+        require('css-mqpacker'),
+        require('cssnano')
+    ]
+};
+
+// CSS processing
+gulp.task('css', ['images'], () => {
+    return gulp.src(css.src)
+        .pipe(sass(css.sassOpts))
+        .pipe(postcss(css.processors))
+        .pipe(gulp.dest(css.build))
+        .pipe(browsersync ? browsersync.reload({ stream: true }) : gutil.noop());
+});
+
+
+// JavaScript settings
+const js = {
+    src: dir.src + 'js/**/*',
+    build: dir.build + 'js/',
+    filename: 'scripts.js'
+};
+
+// JavaScript processing
+gulp.task('js', () => {
+
+    return gulp.src(js.src)
+        .pipe(deporder())
+        .pipe(concat(js.filename))
+        .pipe(stripdebug())
+        .pipe(uglify())
+        .pipe(gulp.dest(js.build))
+        .pipe(browsersync ? browsersync.reload({ stream: true }) : gutil.noop());
+
+});
+
+// copy PHP files
+gulp.task('php', () => {
+    return gulp.src(php.src)
+        .pipe(newer(php.build))
+        .pipe(gulp.dest(php.build));
+});
+
+// run all tasks
+gulp.task('build', ['php', 'css', 'js']);
